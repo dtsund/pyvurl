6958aa082f4e983c6fe81e98923be3c36577925e
Derrick Sund <dtsund@email.arizona.edu>
2014-08-16 21:42:36 -0400

    Implement vurl command, do some tidying.

--- vurl.py
+++ vurl.py
@@ -2,6 +2,7 @@
 import irclib
 import random
 import re
+import string
 
 """
 vurl.py, based on Tim Goodwyn's vurl.pl.
@@ -42,11 +43,48 @@ channel = "##arena-test"
 nick = "pyvurl"
 name = "Vurlybrace"
 
+#Load some global stuff.
+verbs = open("verbs.txt","r").readlines()
+adverbs = open("adverbs.txt","r").readlines()
+
 #Create an IRC object
 irc = irclib.IRC()
 
+def vurl(connection, event):
+    origin = to_vurl = event.source().split("!")[0]
+    if len(event.arguments()[0].split(" ")) > 1:
+        to_vurl = str.lstrip(event.arguments()[0].split(" ",1)[1])
+        #Original impl checked whether there actually is a "me" in the channel.
+        #I think not doing so is potentially funnier.
+        if to_vurl == "me":
+            to_vurl = origin
+    else:
+        to_vurl = origin
+    verb = str.strip(random.choice(verbs))
+    adverb = str.strip(random.choice(adverbs))
+
+    #&(name) should expand to the caller in both verb and adverb
+    verb = re.sub(r"&\(name\)", origin, verb)
+    adverb = re.sub(r"&\(name\)", origin, adverb)
+    to_return = ""
+    #Search and replace &(_) with the vurlee.
+    if re.search(r"&\(_\)", verb):
+        to_return = re.sub(r"&\(_\)", to_vurl, verb)
+    else:
+        to_return = verb + " " + to_vurl
+
+    #If the adverb opens with punctuation attaching it directly to the end
+    #of previous, don't put a space before the adverb.
+    if re.search(r"^[\.,;'!\?]", adverb):
+        to_return = to_return + adverb
+    else:
+        to_return = to_return + " " + adverb
+
+    connection.action(event.target().split("!")[0], to_return)
+    return ""
+
 #This function doesn't even pretend to follow 80cols.
-def hanftl(args):
+def hanftl(connection, event):
     quotelist = ["OH GOD NO",
 	             "HOW CAN EVERYTHING BE ON FIRE AT ONCE?",
 	             "WHY DOES THIS AUTO-SCOUT HAVE TWO ION BOMBS?",
@@ -88,15 +126,19 @@ def hanftl(args):
 	             "HOLY SHIT ENEMY SHIP WITH BURST LASER THREE AND TWO BEAM DRONES IN AN ASTEROID FIELD WHERE DID ALL MY HULL POINTS GO"]
     return random.choice(quotelist)
 
-def homre(args):
+def homre(connection, event):
     return "In trouble!  DRIVE!  DRIVE!  DRIVE!"
 
 #This is where items get added to funclist.
 #Triggers that should be the entire string are formatted as such, for !trigger:
 #^!trigger$
+#Triggers that should be at the beginning of the string, but may contain other
+#stuff afterward, should be as follows:
+#^!trigger
 funclist = []
 funclist.append(TriggerFunction("^!hanftl$", hanftl))
 funclist.append(TriggerFunction("homre", homre))
+funclist.append(TriggerFunction("^!vurl", vurl))
 
 
 def handle_echo(connection, event):
@@ -115,23 +157,18 @@ def handle_pub_msg(connection, event):
     #event.arguments()[0]: The whole message.
     #TODO: check event.source() to make sure vurl doesn't trigger herself.
     #TODO: Make sure empty strings aren't printed.
+    #Redirect event.target in the case of private messages, so that the
+    #response goes to the sender.
+    if event.target() == connection.get_nickname():
+        event.target = event.source
     for func in funclist:
         if re.search(func.trigger, event.arguments()[0]):
             connection.privmsg(event.target().split("!")[0],
-                               func.function(event.arguments()[0]))
-
-def handle_priv_msg(connection, event):
-    #event.arguments()[0]: The whole message.
-    #TODO: check event.source() to make sure vurl doesn't trigger herself.
-    #TODO: Make sure empty strings aren't printed.
-    for func in funclist:
-        if re.search(func.trigger, event.arguments()[0]):
-            connection.privmsg(event.source().split("!")[0],
-                               func.function(event.arguments()[0]))
+                               func.function(connection, event))
 
 #Register event handlers
 irc.add_global_handler("pubmsg", handle_pub_msg)
-irc.add_global_handler("privmsg", handle_priv_msg)
+irc.add_global_handler("privmsg", handle_pub_msg)
 
 
 #Create a server object, connect and join the channel
